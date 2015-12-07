28f48dae89793c0aaf73316bb9b5b9c976984f61
motek96 <tborowicz@vp.pl>
2015-12-07 14:44:53 +0100

    prog_5

---
+++ main_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int n, w;
+string wyraz;
+
+int main()
+{
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        cin >> wyraz;
+        for(int i=0; i<(wyraz.size()/2); i++)
+        {
+            cout << wyraz[i];
+        }
+        cout << endl;
+    }
+}
